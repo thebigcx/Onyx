e063fc7d1c463907ddd60e48cc82d3e533f1e887
thebicx <christianprince132@gmail.com>
2021-03-06 11:10:51 +1100

    vendor

---
+++ src/onyx/vendor/dr_libs.cpp
@@ -0,0 +1,8 @@
+#define DR_WAV_IMPLEMENTATION
+#include <dr_libs/dr_wav.h>
+
+#define DR_MP3_IMPLEMENTATION
+#include <dr_libs/dr_mp3.h>
+
+#define DR_FLAC_IMPLEMENTATION
+#include <dr_libs/dr_flac.h>
